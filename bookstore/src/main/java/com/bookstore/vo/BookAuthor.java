87f11a03eb19e11337ed88fa8f8414c507afb2e1
zhouweihui <2396562901@qq.com>
2019-02-22 15:49:45 +0800

    boostore vo 类

---
+++ bookstore/src/main/java/com/bookstore/vo/BookAuthor.java
@@ -0,0 +1,62 @@
+package com.bookstore.vo;
+
+public class BookAuthor {
+
+	private String bookId;
+	private String firstName;
+	private String lastName;
+	private String street;
+	private String city;
+	private String title;
+	private String description;
+	private String longDescription;
+	
+	public String getBookId() {
+		return bookId;
+	}
+	public void setBookId(String bookId) {
+		this.bookId = bookId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getLongDescription() {
+		return longDescription;
+	}
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
+	}
+}
